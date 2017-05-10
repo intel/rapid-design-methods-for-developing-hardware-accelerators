eee72806bd63b76c0901d808bc9ee3aaad054247
Burns, Steven M <steven.m.burns@intel.com>
2017-05-10 13:24:13 -0700

    Replaced AST with imperativeAST

--- src/main/scala/AST.scala
+++ src/main/scala/AST.scala
@@ -1,20 +1,33 @@
-package imperative_dont_use
+package imperative
 
-abstract class AST
+import scala.util.parsing.input.Positional
 
-abstract class Command extends AST
-abstract class Expression extends AST
-abstract class BExpression extends AST
+sealed trait Command extends Positional
+sealed trait Expression extends Positional
+sealed trait BExpression extends Positional
+sealed trait Dir extends Positional
+sealed trait Type extends Positional
 
-case class Port( nm : String) extends Expression
+case class Port( nm : String) extends Positional
 case class Variable( nm : String) extends Expression
 case class ConstantInteger( c : Int) extends Expression
 case object ConstantTrue extends BExpression
 
+case object Inp extends Dir
+case object Out extends Dir
+
+case class Process( lst : PortDeclList, cmd : Command) extends Positional
+
+case class UIntType( width : Int) extends Type
+case class VecType( n : Int, t : Type) extends Type
+
+case class Decl( v : Variable, t : Type) extends Positional
+case class PortDecl( p : Port, dir : Dir, t : Type) extends Positional
+case class PortDeclList( lst : List[PortDecl]) extends Positional
 case class Assignment( lhs : Variable, rhs : Expression) extends Command
 case class While( cond : BExpression, body : Command) extends Command
 case class IfThenElse( cond : BExpression, bodyT : Command, bodyF : Command) extends Command
-case class SequentialComposition( seq : Seq[Command]) extends Command
+case class Blk( decls : Seq[Decl], seq : Seq[Command]) extends Command
 case class AddExpression( l : Expression, r : Expression) extends Expression
 case class MulExpression( l : Expression, r : Expression) extends Expression
 case class EqBExpression( l : Expression, r : Expression) extends BExpression
@@ -23,6 +36,5 @@ case class NotBExpression( e : BExpression) extends BExpression
 case object Wait extends Command
 case class NBCanGet( p : Port) extends BExpression
 case class NBCanPut( p : Port) extends BExpression
-case class NBGetData( p : Port) extends Expression
-case class NBGet( p : Port) extends Command
+case class NBGet( p : Port, v : Variable) extends Command
 case class NBPut( p : Port, e : Expression) extends Command
