a415440149fe238642adf465993fc8c868ef0a17
Burns, Steven M <steven.m.burns@intel.com>
2017-05-25 12:49:03 -0700

    Fast implementation of flatten using an accumulating parameter

--- src/test/scala/F.scala
+++ src/test/scala/F.scala
@@ -7,7 +7,7 @@ import org.scalameter.api._
 import scala.annotation.tailrec
 
 class F
-case class FSeq( lst : Seq[F]) extends F
+case class FSeq( lst : List[F]) extends F
 case class FPrim( x : Int) extends F
 
 object F {
@@ -19,32 +19,53 @@ object F {
     case FPrim( _) => FSeq( List(f))
   }
 
-  def doit = flatten( FSeq( List( FSeq( List( FSeq( List( FPrim(0))), FPrim(1))), FPrim(2))))
+  def flatten2( f : F) : FSeq = {
+    def aux( accum : List[F], f : F) : List[F] = f match {
+      case FPrim( _) => f :: accum
+      case FSeq( hd::tl) => aux( aux( accum, hd), FSeq( tl))
+      case FSeq( Nil) => accum
+    }
+    FSeq( aux( List(), f).reverse)
+  }
+
+
+  def doit( func : F => FSeq) =
+    func( FSeq( List( FSeq( List( FSeq( List( FPrim(0))), FPrim(1))), FPrim(2))))
 
 }
 
 class FTest extends FreeSpec with Matchers {
   "F" - {
-    "F.doit works" in {
-      F.doit should be ( FSeq(List(FPrim(0), FPrim(1), FPrim(2))))
+    "F.flatten works" in {
+      F.doit{ F.flatten} should be ( FSeq(List(FPrim(0), FPrim(1), FPrim(2))))
     }
-    "F slugbug shouldn't take too much time" in {
-      val n = 30000
-      F.flatten( FSeq( (for( i <- 0 until n) yield FSeq( List( FPrim(i)))))) should be (
-         FSeq( (for( i <- 0 until n) yield FPrim( i))))
+    "F.flatten2 works" in {
+      F.doit{ F.flatten2} should be ( FSeq(List(FPrim(0), FPrim(1), FPrim(2))))
+    }
+    "flatten shouldn't take too much time" in {
+      val n = 3000
+      F.flatten( FSeq( (for( i <- 0 until n) yield FSeq( List( FPrim(i)))).toList)) should be (
+         FSeq( (for( i <- 0 until n) yield FPrim( i)).toList))
+    }
+    "flatten2 shouldn't take too much time" in {
+      val n = 3000
+      F.flatten2( FSeq( (for( i <- 0 until n) yield FSeq( List( FPrim(i)))).toList)) should be (
+         FSeq( (for( i <- 0 until n) yield FPrim( i)).toList))
     }
   }
 }
 
-object RangeBenchmark extends Bench.LocalTime {
+object FBenchmark extends Bench.LocalTime {
   val sizes = Gen.exponential("size")( 128, 8*1024, 2)
   val ranges = for {
     n <- sizes
-  } yield FSeq( (for( i <- 0 until n) yield FSeq( List( FPrim(i)))))
-  println( s"${ranges}")
-  performance of "Range" in {
-    measure method "F.flatten" in {
+  } yield FSeq( (for( i <- 0 until n) yield FSeq( List( FPrim(i)))).toList)
+  performance of "F" in {
+    measure method "flatten" in {
       using(ranges) in { r => { F.flatten( r); ()}}
+    }
+    measure method "flatten2" in {
+      using(ranges) in { r => { F.flatten2( r); ()}}
     }
   }
 }
